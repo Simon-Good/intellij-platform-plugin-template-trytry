fec148dface4abb5bfa205b7b23aca6f21f4dc4d
GitHub Action <action@github.com>
2021-10-11 04:32:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/simongood/intellijplatformplugintemplatetrytry/MyBundle.kt
+++ src/main/kotlin/com/github/simongood/intellijplatformplugintemplatetrytry/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.simongood.intellijplatformplugintemplatetrytry
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/simongood/intellijplatformplugintemplatetrytry/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.simongood.intellijplatformplugintemplatetrytry.services
+
+import com.github.simongood.intellijplatformplugintemplatetrytry.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
